16b983e68416963eabf2620eb42a1bdfb186b379
faysalfahim <faysalfahim795@gmail.com>
2024-04-26 06:12:10 +0600

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Login from './pages/login/login';
-import ListComponent from './pages/movie-list/list';
+import MovieListComponent from './pages/movie-list/list';
 import MovieDetails from './pages/movie-details/movie-details';
 import FavouriteMovies from './pages/favourite-movies/list';
 
@@ -11,7 +11,7 @@ function App() {
     <Router>
      <Routes>
           <Route exact path="/" element={<Login/>}/>
-          <Route exact path="/list" element={<ListComponent/>}/>
+          <Route exact path="/list" element={<MovieListComponent/>}/>
           <Route exact path="/details/:id" element={<MovieDetails/>} />
           <Route exact path="/favourites" element={<FavouriteMovies/>} />
         </Routes>
